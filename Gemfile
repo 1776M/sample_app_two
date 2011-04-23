3f1c1cec25e1f7f062d0540ecc639600aaf92cd5
Mandeep Khera <world.mover@gmail.com>
2011-04-23 13:41:52 +0000

    User model with passwords, and user show page

--- Gemfile
+++ Gemfile
@@ -2,6 +2,7 @@ source 'http://rubygems.org'
 
 gem 'rails', '3.0.6'
 gem 'sqlite3-ruby', '1.3.2', :require => 'sqlite3'
+gem 'gravatar_image_tag', '1.0.0.pre2'
 
 group :development do
   gem 'rspec-rails', '2.5.0'
@@ -11,4 +12,5 @@ end
 group :test do
   gem 'rspec', '2.5.0'
   gem 'webrat', '0.7.1'
+  gem 'factory_girl_rails', '1.0'
 end
--- db/schema.rb
+++ db/schema.rb
@@ -10,13 +10,15 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20110423102816) do
+ActiveRecord::Schema.define(:version => 20110423125551) do
 
   create_table "users", :force => true do |t|
     t.string   "name"
     t.string   "email"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "encrypted_password"
+    t.string   "salt"
   end
 
 end
